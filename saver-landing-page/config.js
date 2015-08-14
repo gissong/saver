2da78737709216e9a44e2e76288565d1c47956c6
gissong <songgaogeo@gmail.com>
2015-08-14 14:59:37 -0700

    Esri Saver Website

---
+++ saver-landing-page/config.js
@@ -0,0 +1,86 @@
+var configObj = {
+	title: "Esri Saver",
+	subtitle: "Explore More. Save More. ",
+	author: "3D Badgers",
+	appIcon: "assets/icons/icon.png",
+
+	// The big background for your home page
+	mainBackground: "assets/backgrounds/Badgers_1.png",
+
+	// A screenshot of your app showcased on the home page
+	featuredScreenshot: "assets/screenshots/1.png",
+
+	// what type of device you want to show your featured screenshot in.
+	// valid answers are 'phone', 'tablet', 'laptop', and 'desktop'.
+	shimDevice: 'phone',
+	
+	// whether you want the device to be horizontal or vertical.
+	// valid answers are 'portrait' and 'landscape' 
+	shimOrientation: 'portrait', 
+	
+	// links to the apps -- enter the ones you have platforms for.
+	androidLink: "https://play.google.com/store/apps/details?id=com.esri.palmspringsmaptour",
+	iOSLink: "https://itunes.apple.com/us/app/palm-springs-map-tour/id971338647?ls=1&mt=8",
+	webAppLink: "http://mediamaps.esri.com/discounts/",
+	windows8Link: "",
+	osXLink: "", 
+    
+	// Features page subtitle
+	featuresBlurb: "Use this fun app to discover Esri local discounts.",
+	// Features page screenshots (in a carousel)
+	screenshots: [
+		"assets/screenshots/1.png",
+		"assets/screenshots/2.png",
+		"assets/screenshots/3.png",
+		"assets/screenshots/4.png",
+		"assets/screenshots/5.png",
+		"assets/screenshots/6.png"
+	],
+	
+	// bullet-point of features your app has
+	features: [		
+		"Interactive Map",
+		"Location Awareness",
+		"Simple & Easy to Use",				
+		"Runs on Any Device"
+	],
+
+	// about section describing your app.
+	aboutParagraph: "This Saver app contains awesomeness of being part of Esri. It helps new employees & interns easily explore the Esri local benefits (including food, services, entertainment, healthcare etc.) based on where they are. It also provides valuable feedbacks to Esri on who are the best local businesses to partner with.",
+
+	// formal name of your organization/company
+	organizationName: "This app is built using the",
+	// the city and abbreviated state for your organization
+	organizationCityAndState: "new",
+	organizationWebsite: "AppStudio for ArcGIS",
+
+	// a link to a promotional video
+	videoUrl: "",
+
+	// yes or no to social links
+	showCommunity: "no",
+
+	// the term used in the feed you want
+	twitterSearchTerm: "appstudio",
+
+	// the id of the twitter feed. (You have to create the feed through twitter first to get this id.)
+	twitterWidgetId: "",
+
+	facebookLink: "",
+	twitterLink: "",	
+	blogLink: "",
+
+
+	// stylings
+	// each page is an alternating color.
+	primaryBackgroundColor: "FFF",
+	secondaryBackgroundColor: "EEE"
+};
+
+
+
+
+	
+
+
+
